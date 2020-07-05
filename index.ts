0ac471ef62d67eb325e20e9be1b13c474495bf81
hhhhhq <Mr.j@tom.com>
2020-07-05 10:11:45 +0800

    init

--- index.ts
+++ index.ts
@@ -4,5 +4,27 @@
 
 type User = {
   name: string,
+  age: number,
+  print(): void
+}
+
+const user: User = {
+  name: "Jld",
+  age: 29,
+  print() {}
+}
+
+interface IUser {
+  name: string
   age: number
+}
+
+interface IUser {
+  email: string
+}
+
+const other_user: IUser = {
+  name: "Jld",
+  age: 29,
+  email: "Mr.J@tom.com"
 }
