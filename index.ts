4ddc2679fef787755eda43e2d76454c91cffceb0
hhhhhq <Mr.j@tom.com>
2020-07-06 12:05:40 +0800

    finished

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 
 interface Person {
-  first_name: string
+  // 只读 不可修改
+  readonly first_name: string
   last_name?: string
 
   print?(): void
@@ -8,25 +9,25 @@ interface Person {
   [propName: string]: any
 }
 
-let person = {
-  first_name: "Jld",
-  // last_name: "love",
-  // age: 30
-}
+// let person = {
+//   first_name: "Jld",
+//   // last_name: "love",
+//   // age: 30
+// }
 
 class Programmer implements Person {
   first_name: string
 }
 
-const sayName = (o: Person) => {
-  console.log(o.first_name + o.last_name)
-}
+// const sayName = (o: Person) => {
+//   console.log(o.first_name + o.last_name)
+// }
 
-const programmer = new Programmer()
+const programmer: Person = new Programmer()
 programmer.first_name = "Jld1"
 
 // 类型断言
 // sayName({first_name: "Jld2", last_name: "love", age: 30} as Person)
 // sayName(programmer)
 
-sayName({first_name: "Jld2", lasdddt_name: "love", age: 30})
+// sayName({first_name: "Jld2", lasdddt_name: "love", age: 30})
