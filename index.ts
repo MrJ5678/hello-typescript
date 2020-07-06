e6a19769df05a2f3d1b16f992ed49cce99efd0a4
hhhhhq <Mr.j@tom.com>
2020-07-06 12:31:48 +0800

    finished

--- index.ts
+++ index.ts
@@ -4,27 +4,42 @@ interface Person {
   readonly first_name: string
   last_name?: string
 
-  print?(): void
+  print(callback: PrintCallback): void
 
   [propName: string]: any
 }
 
-// let person = {
-//   first_name: "Jld",
-//   // last_name: "love",
-//   // age: 30
-// }
-
-class Programmer implements Person {
-  first_name: string
+interface PrintCallback {
+  // 可以看作是匿名函数
+  (success: boolean): void
 }
+
+let printCallback: PrintCallback
+printCallback = (suc: boolean): void => {
+  console.log("callback", suc)
+}
+
+let person = {
+  first_name: "Jld",
+  // last_name: "love",
+  age: 30,
+  print(callback: PrintCallback): void {
+    callback(true)
+  }
+}
+
+person.print(printCallback)
+
+// class Programmer implements Person {
+//   first_name: string
+// }
 
 // const sayName = (o: Person) => {
 //   console.log(o.first_name + o.last_name)
 // }
 
-const programmer: Person = new Programmer()
-programmer.first_name = "Jld1"
+// const programmer: Person = new Programmer()
+// programmer.first_name = "Jld1"
 
 // 类型断言
 // sayName({first_name: "Jld2", last_name: "love", age: 30} as Person)
