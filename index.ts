051dc2f60334c73f44da84fd56920389376f0b67
hhhhhq <Mr.j@tom.com>
2020-07-14 09:09:00 +0800

    init

--- index.ts
+++ index.ts
@@ -1,60 +1,83 @@
-// 泛型class
+// // 泛型class
 
-class List<T> {
-  private data: T[]
+// class List<T> {
+//   private data: T[]
 
-  constructor(elements: T[]) {
-    this.data = elements
-  }
+//   constructor(elements: T[]) {
+//     this.data = elements
+//   }
 
-  add(t: T) {
-    this.data.push(t)
-  }
+//   add(t: T) {
+//     this.data.push(t)
+//   }
 
-  remove(t: T) {
-    let index = this.data.indexOf(t)
-    if(index > -1) {
-      this.data.splice(index, 1)
-    }
-  }
+//   remove(t: T) {
+//     let index = this.data.indexOf(t)
+//     if(index > -1) {
+//       this.data.splice(index, 1)
+//     }
+//   }
 
-  asArray(): T[] {
-    return this.data
-  }
-}
+//   asArray(): T[] {
+//     return this.data
+//   }
+// }
 
-let numbers = new List<number>([1, 2, 3, 4, 5])
-numbers.add(6)
-numbers.remove(3)
-let numArray = numbers.asArray()
+// let numbers = new List<number>([1, 2, 3, 4, 5])
+// numbers.add(6)
+// numbers.remove(3)
+// let numArray = numbers.asArray()
 
-// console.log(numArray)
+// // console.log(numArray)
 
-let fruits = new List<string>(["apple", "banana", "peach"])
-fruits.add("pair")
-fruits.remove("banana")
-let fruitsArr = fruits.asArray()
+// let fruits = new List<string>(["apple", "banana", "peach"])
+// fruits.add("pair")
+// fruits.remove("banana")
+// let fruitsArr = fruits.asArray()
 
-// console.log(fruitsArr)
+// // console.log(fruitsArr)
+
+// class Pair<F, S> {
+//   private _first: F
+//   private _second: S
+
+//   constructor(first: F, second: S) {
+//     this._first = first
+//     this._second = second
+//   }
+
+//   get first(): F {
+//     return this._first
+//   }
+
+//   get second(): S {
+//     return this._second
+//   }
+// }
+
+// let pair = new Pair<boolean, string>(true, "Jld")
+// console.log(pair.first)
+// console.log(pair.second)
 
 class Pair<F, S> {
-  private _first: F
-  private _second: S
+  first: F;
+  second: S;
 
   constructor(first: F, second: S) {
-    this._first = first
-    this._second = second
-  }
-
-  get first(): F {
-    return this._first
-  }
-
-  get second(): S {
-    return this._second
+      this.first = first;
+      this.second = second;
   }
 }
 
-let pair = new Pair<boolean, string>(true, "Jld")
-console.log(pair.first)
-console.log(pair.second)
+function getFirstArray<F, S>(pairs: Pair<F, S>[]): F[] {
+  let arr: F[] = [];
+  for (let i = 0; i < pairs.length; i++) {
+      let first: F = pairs[i].first;
+      arr.push(first);
+  }
+  return arr;
+}
+
+let numArray: Pair<number, boolean>[] = [new Pair(1, true), new Pair(2, false)]
+
+console.log(getFirstArray(numArray))
