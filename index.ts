596172130c580f97790a7a848082f688441c154d
hhhhhq <Mr.j@tom.com>
2020-07-14 09:47:48 +0800

    init

--- index.ts
+++ index.ts
@@ -1,54 +1,24 @@
-
-interface Person {
-  // 只读 不可修改
-  readonly first_name: string
-  last_name: string
-
-  // print(callback: PrintCallback): void
-
-  // [propName: string]: any
-}
-
-// let x: any = 'hi hi'
-// // <string> 是告诉编译器 此时x看作字符串
-// let s = (<string>x).substring(0,3)
-// console.log(s)
-
-/* interface PrintCallback {
-  // 可以看作是匿名函数
-  (success: boolean): void
-}
-
-let printCallback: PrintCallback
-printCallback = (suc: boolean): void => {
-  console.log("callback", suc)
-}
-
-let person = {
-  first_name: "Jld",
-  // last_name: "love",
-  age: 30,
-  print(callback: PrintCallback): void {
-    callback(true)
-  }
-}
-
-person.print(printCallback) */
-
-// class Programmer implements Person {
-//   first_name: string
+// interface States<R> {
+//   [state: string]: R
 // }
 
-const sayName = (o: Person) => {
-  console.log(o.first_name + o.last_name)
+// let s: States<boolean> = { 'enabled': true, 'maximized': false }
+// console.log(s)
+// console.log(s['maximized'])
+
+interface Pair<F, S> {
+  first: F
+  second: S
 }
 
-// const programmer: Person = new Programmer()
-// programmer.first_name = "Jld1"
+interface States<F, S> {
+  [state: string]: Pair<F, S>
+}
 
-// 类型断言
-// sayName({first_name: "Jld2", last_name: "love", age: 30} as Person)
-sayName({first_name: "Jld2", last_name: "love", age: 30} as Person)
-// sayName(programmer)
+let s: States<number, boolean> = {
+  'enabled': { first: 1, second: true },
+  'maximized': { first: 2, second: false }
+}
 
-// sayName({first_name: "Jld2", lasdddt_name: "love", age: 30})
+console.log(s)
+console.log(s['maximized'])
