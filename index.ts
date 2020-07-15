d5b5964a8a70353d9f41eabbcef2964a36af62e4
hhhhhq <Mr.j@tom.com>
2020-07-15 09:48:00 +0800

    init

--- index.ts
+++ index.ts
@@ -1,54 +1,26 @@
-
-interface Person {
-  // 只读 不可修改
-  readonly first_name: string
-  last_name: string
-
-  // print(callback: PrintCallback): void
-
-  // [propName: string]: any
+class A {
+  x: number = 5
+  c: string
 }
 
-// let x: any = 'hi hi'
-// // <string> 是告诉编译器 此时x看作字符串
-// let s = (<string>x).substring(0,3)
-// console.log(s)
+let y: keyof A
+y = "c"
 
-/* interface PrintCallback {
-  // 可以看作是匿名函数
-  (success: boolean): void
+class Test {
+  x: number = 6
 }
 
-let printCallback: PrintCallback
-printCallback = (suc: boolean): void => {
-  console.log("callback", suc)
+function getProp(a: keyof Test, test: Test): any {
+  return test[a]
 }
 
-let person = {
-  first_name: "Jld",
-  // last_name: "love",
-  age: 30,
-  print(callback: PrintCallback): void {
-    callback(true)
-  }
+let t: Test = new Test()
+let prop = getProp('x', t)
+console.log(prop)
+
+class B {
+  y: keyof A
 }
 
-person.print(printCallback) */
-
-// class Programmer implements Person {
-//   first_name: string
-// }
-
-const sayName = (o: Person) => {
-  console.log(o.first_name + o.last_name)
-}
-
-// const programmer: Person = new Programmer()
-// programmer.first_name = "Jld1"
-
-// 类型断言
-// sayName({first_name: "Jld2", last_name: "love", age: 30} as Person)
-sayName({first_name: "Jld2", last_name: "love", age: 30} as Person)
-// sayName(programmer)
-
-// sayName({first_name: "Jld2", lasdddt_name: "love", age: 30})
+let b: B = new B()
+b.y = "x"
